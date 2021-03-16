7915260e60dd78ca385e1634ba071df9c559cfaf
Temirbek <temirbek@gmail.com>
2021-03-16 11:35:30 +0600

    cors

--- modules/api/controllers/BaseController.php
+++ modules/api/controllers/BaseController.php
@@ -3,7 +3,36 @@
 namespace app\modules\api\controllers;
 
 use yii\rest\ActiveController;
+use yii\web\Response;
+use yii\filters\ContentNegotiator;
+use \yii\filters\Cors;
 
 class BaseController extends ActiveController
 {
+    /**
+     * @inheritdoc
+     * @return array
+     */
+    public function behaviors()
+    {
+        /* return ArrayHelper::merge([
+            [
+                'class' => Cors::className(),
+            ],
+        ], parent::behaviors()); */
+
+        $behaviors = parent::behaviors();
+        unset($behaviors['authenticator']);
+        $behaviors['corsFilter'] = [
+            'class' => Cors::className(),
+        ];
+        $behaviors['contentNegotiator'] = [
+            'class' => ContentNegotiator::className(),
+            'formats' => [
+                'application/json' => Response::FORMAT_JSON,
+            ],
+        ];
+
+        return $behaviors;
+    }
 }
